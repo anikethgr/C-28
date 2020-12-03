66dd1724d07efbbb81df85bc17121d526bcb6755
anikethgr <72132558+anikethgr@users.noreply.github.com>
2020-12-03 14:38:36 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,30 @@
+class   Slingshot{
+        constructor(bodyA, newPointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: newPointB,
+            stiffness: 0.04,
+            length: 10
+        
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+    fly(){
+        this.sling.bodyA = null;
+
+
+    }
+
+}
+
